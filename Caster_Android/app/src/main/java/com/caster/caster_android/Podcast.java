6548a4549144f2d17345c1bc561c35a86e6afba1
Nick Morrison <nickmorrison09@gmail.com>
2015-05-05 23:01:54 -0400

    Implemented the ability to retrieve podcast metadata from the servers

--- Caster_Android/app/src/main/java/com/caster/caster_android/Podcast.java
+++ Caster_Android/app/src/main/java/com/caster/caster_android/Podcast.java
@@ -2,25 +2,63 @@ package com.caster.caster_android;
 
 import android.graphics.Bitmap;
 
-import java.util.Dictionary;
+import java.util.HashMap;
 
 /**
  * Created by Nick on 15-05-05.
+ *
+ * Podcast
+ * Used to store the metadata and image for the Podcast
+ * Can be created using Podcast.makeFromID(<id>) to retrieve one from the database.
+ * holds information such as the coverphoto url, title, description, views, likes, length,
+ * podcast id and creator id.
  */
 public class Podcast {
 
     public final static int COVERPHOTO = 0, TITLE = 1, DESCRIPTION = 2, VIEWS = 3, LIKES = 4,
-                            LENGTH = 5;
+                            LENGTH = 5, ID=6,CREATOR_ID=7;
 
-    Dictionary<Integer,Object> metadata; //image,title, descriptions, views, likes, length, etc.
+    HashMap<Integer, Object> metadata; //image,title, descriptions, views, likes, length, etc.
     User creator;
 
-    public Podcast(User creator,Dictionary<Integer,Object> metadata){
+    /***
+     * Retrieve the podcast's metadata from the server
+     * @param podcastid - the podcast's id
+     * @return - an instance of the podcast
+     */
+    public static Podcast makeFromID(int podcastid){
+        HashMap<Integer,Object> data = new HashMap<>();
+        Podcast re = null;
+        String urlStr = "http://192.168.2.155:8000/php/podcast.php";
+        try {
+            CasterRequest request = new CasterRequest(urlStr);
+            String res = (String)request.addParam("q","TTL").addParam("id",""+podcastid).execute().get();
+            request = new CasterRequest(urlStr);
+            data.put(TITLE, res);
+            res = (String)request.addParam("q","LNGTH").addParam("id",""+podcastid).execute().get();
+            request = new CasterRequest(urlStr);
+            data.put(LENGTH, Integer.parseInt(res));
+            res = (String)request.addParam("q","DESC").addParam("id",""+podcastid).execute().get();
+            request = new CasterRequest(urlStr);
+            data.put(DESCRIPTION, res);
+            data.put(ID,podcastid);
+            res = (String)request.addParam("q","USR").addParam("id",""+podcastid).execute().get();
+            data.put(CREATOR_ID,Integer.parseInt(res));
+            //String imgname = result.getString("image_file");
+            //String imgURL = "users/"+(String)data.get(CREATOR_ID)+"/images/podcast/"+imgname;
+            re = new Podcast(data);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return re;
+    }
+
+    public Podcast(User creator,HashMap<Integer, Object> metadata){
         this.creator = creator;
         this.metadata = metadata;
     }
 
-    public Podcast(Dictionary<Integer,Object> metadata){
+    public Podcast(HashMap<Integer, Object> metadata){
         this.metadata = metadata;
         this.creator = null;
     }
@@ -53,7 +91,16 @@ public class Podcast {
         return (int)metadata.get(LENGTH);
     }
 
-    public Dictionary<Integer,Object> getMetadata(){
+    public int getId(){
+        return (int)metadata.get(LENGTH);
+    }
+
+    public int getCreatorId(){
+        return (int)metadata.get(CREATOR_ID);
+    }
+
+
+    public HashMap<Integer, Object> getMetadata(){
         return  metadata;
     }
 
