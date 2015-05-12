c2a9ed101d1cbf924d03d9b0d53d5330190306bc
Nickolas A. Morrison <nickmorrison09@gmail.com>
2015-05-12 13:08:03 -0400

    Added logo to actionbar and started working on navigation drawer.

--- Caster_Android/app/src/main/java/com/caster/caster_android/MainActivity.java
+++ Caster_Android/app/src/main/java/com/caster/caster_android/MainActivity.java
@@ -1,18 +1,45 @@
 package com.caster.caster_android;
 
+import android.app.ActionBar;
+import android.app.Activity;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
+import android.support.v4.widget.DrawerLayout;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ListView;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
     public static String host = "jdbc:mysql://192.168.2.155:8000/caster_db?autoReconnect=true";
+    public static MainActivity instance;
+
+    private DrawerLayout drawerLayout;
+    private ListView drawerList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        instance = this;
+
+        ActionBar actionBar = getActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(false);
+        actionBar.setDisplayShowHomeEnabled(false);
+        actionBar.setDisplayShowTitleEnabled(false);
+        actionBar.setDisplayShowCustomEnabled(true);
+        LayoutInflater inflator = getLayoutInflater();
+
+        View customView = inflator.inflate(R.layout.actionbar_layout, null);
+        ActionBar.LayoutParams params = new ActionBar.LayoutParams(ActionBar.LayoutParams.MATCH_PARENT,
+                ActionBar.LayoutParams.MATCH_PARENT,
+                Gravity.CENTER_HORIZONTAL);
+        actionBar.setCustomView(customView,params);
+
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawerList = (ListView) findViewById(R.id.left_drawer);
         /*
         // Testing backend stuff
         User user = User.makeFromID(24);
@@ -25,7 +52,7 @@ public class MainActivity extends ActionBarActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        //getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
