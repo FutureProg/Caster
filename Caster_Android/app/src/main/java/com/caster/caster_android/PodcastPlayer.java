b1009abd06bf69ad891389a91a93353c72cee13b
teovoinea <voinea.teodor@gmail.com>
2015-08-07 12:50:01 -0400

    Subscription

--- Caster_Android/app/src/main/java/com/caster/caster_android/PodcastPlayer.java
+++ Caster_Android/app/src/main/java/com/caster/caster_android/PodcastPlayer.java
@@ -1,6 +1,7 @@
 package com.caster.caster_android;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
@@ -18,6 +19,7 @@ import android.widget.TextView;
 import com.caster.caster_android.utils.Bin;
 
 import java.io.IOException;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 
 
@@ -244,5 +246,47 @@ public class PodcastPlayer extends Activity {
         //Checks whether the user is subscribed, if subscribed, then display the correct button
         return false;
     }
+
+    public void subscribe(View v){
+        Button subscribe = ((Button)findViewById(R.id.subscribe));
+        //UnSubscribing
+        if (subscribe.getBackground().getConstantState().equals(getResources().getDrawable(R.drawable.unsubscribe))) {
+            try {
+                if (Bin.unsubscribe(podcast.getId()) == false) {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                    builder.setMessage("Error UnSubscribing, Are you signed in?").setTitle("Message");
+                    builder.create().show();
+                    return;
+                } else {
+                    subscribe.setBackground(getResources().getDrawable(R.drawable.subscribe));
+                }
+            } catch (ExecutionException e) {
+                e.printStackTrace();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        //Subscribing
+        else{
+            try{
+                if(Bin.subscribe(podcast.getId()) == false){
+                    AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                    builder.setMessage("Error Subscribing, Are you signed in?").setTitle("Message");
+                    builder.create().show();
+                    return;
+                }
+                else{
+                    subscribe.setBackground(getResources().getDrawable(R.drawable.unsubscribe));
+                }
+            }
+            catch (ExecutionException e){
+                e.printStackTrace();
+            }
+            catch (InterruptedException e){
+                e.printStackTrace();
+            }
+        }
+    }
+
 
 }
