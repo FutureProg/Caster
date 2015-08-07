c14c6136d8339a22ea051a53e426783f11ea24dd
Nick Morrison <nickmorrison09@gmail.com>
2015-08-07 12:26:18 -0400

    Update subscription.php

--- Caster_website/php/subscription.php
+++ Caster_website/php/subscription.php
@@ -14,6 +14,19 @@ else if($q == "UNSUB"){
 else if($q == "CHECK"){
     check();
 }
+else if($q == "MOBI"){
+	$t = filter_input(INPUT_POST, "t");
+	if($t == "SUB"){
+		mobi_sub();
+	}
+	else if($t == "UNSUB"){
+		mobi_unsub();
+	}
+	else if($t == "CHECK"){
+		mobi_check();
+	}
+}
+
 
 function check(){
     $subscribe_id = filter_input(INPUT_POST,"s");
@@ -78,4 +91,66 @@ function unsubscribe(){
     mysqli_close($link);
 }
 
+function mobi_sub(){
+	$subscribe_id = filter_input(INPUT_POST, "s");
+	$user_id = filter_input(INPUT_POST, "u");
+	$link = mysqli_connect(DB_HOST,DB_USER,DB_PASSWORD,DB_NAME) or die("Error connecting to server");
+    $query = "SELECT `subscriptions` FROM `".TABLE_USERS."` WHERE `user_id`=".$user_id;        
+    $result = mysqli_query($link,$query) or die("Error querying the server");    
+    if(mysqli_num_rows($result) >= 1){
+        echo "OKAY ";
+        $row = mysqli_fetch_array($result);   
+        $string = $subscribe_id.".";
+        echo "/".strpos($row['subscriptions'],$string)."/";        
+        if(strpos($row['subscriptions'],$string) !== FALSE){
+            echo "YES";
+        }
+        else{
+            echo "NO";   
+        }
+    }
+    else{
+        echo "NO";   
+    }
+    mysqli_close($link);
+}
+
+function mobi_unsub(){
+	$subscribe_id = filter_input(INPUT_POST,"s");
+    $user_id = filter_input(INPUT_POST, "u");
+    $link = mysqli_connect(DB_HOST,DB_USER,DB_PASSWORD,DB_NAME) or die("Error connecting to server");    
+    $query = "UPDATE `".TABLE_USERS."` SET subscriptions=CONCAT(subscriptions,'".$subscribe_id.".') WHERE `user_id`=".$user_id;   
+    $result = mysqli_query($link,$query) or die ("Error querying the server");
+    if(mysqli_num_rows($result) >= 1){
+        echo "OKAY";   
+    }
+    else{
+        echo "NO";   
+    }
+    mysqli_close($link);
+}
+
+function mobi_check(){
+	$subscribe_id = filter_input(INPUT_POST,"s");
+    $user_id = filter_input(INPUT_HOST, "u");    
+    $link = mysqli_connect(DB_HOST,DB_USER,DB_PASSWORD,DB_NAME) or die("Error connecting to the server");
+    $query = "SELECT `subscriptions` FROM `".TABLE_USERS."` WHERE `user_id`=".$user_id;        
+    $result = mysqli_query($link,$query) or die("Error querying the server");    
+    if(mysqli_num_rows($result) >= 1){
+        $row = mysqli_fetch_array($result);
+        $newSubs = str_replace($subscribe_id.".","",$row['subscriptions']);
+        $query = "UPDATE `".TABLE_USERS."` SET `subscriptions`='".$newSubs."'";        
+        $result = mysqli_query($link,$query) or die("Error querying the server");
+        if($result){
+            echo "YES";   
+        }
+        else{
+            echo "NO";   
+        }
+    }
+    else{
+        echo "NO";   
+    }
+    mysqli_close($link);
+}
 ?>
