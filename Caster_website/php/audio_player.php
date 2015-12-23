e9ad7245bb9b27ccd03f7e0fcd3e6dbf4ea35e25
Nickolas A. Morrison <nickmorrison09@gmail.com>
2015-12-23 15:35:45 -0500

    Redesigning the audio player

--- Caster_website/php/audio_player.php
+++ Caster_website/php/audio_player.php
@@ -1,30 +1,35 @@
 <!---Start Audio Player--->
 <div id="audio-player">
-    <div id="audio-top-bar">  
-        <button id="play-button"><img src="/images/play_button.png"/></button>    
-        <button class="skip-button" id="skip-back-button"><img src="/images/skip_button.png"/></button>
-        <div id="audio-player-title-area">
-            <div class="click-area">
-                <div id="now-playing">Now Playing:</div>    
-            </div>            
-        </div>
-        <button class="skip-button" id="skip-forward-button"><img src="/images/skip_button.png"/></button>
+    <div id="audio-top-bar" class="click-area">
+
+        <!--<button class="skip-button" id="skip-back-button"><img src="/images/skip_button.png"/></button>-->
+        <!--<button class="skip-button" id="skip-forward-button"><img src="/images/skip_button.png"/></button>-->
         <audio preload="auto">
             <source src="" type="audio/mpeg"/>
-        </audio>        
+        </audio>
     </div>
-    <div id="audio-player-scrubber">        
+    <div id="audio-player-scrubber">
         <div id="audio-player-time-stamp">0:00/0:00</div>
         <div id="audio-scrub-bar">
             <div id="audio-scrub-circle">
             </div>
         </div>
     </div>
-    <div id="audio-player-content">              
-        <div id="audio-player-comment-area">            
+    <div id="audio-player-content">
+        <div id="audio-player-comment-area">
         </div>
     </div>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-        <script src="../js/audioplayer.js"></script>
-</div>
+  </div>
+  <div id="audio-bottom-bar">
+    <image id="podcast-art"></image>
+    <button id="play-button"><img src="/images/play_button.png"/></button>
+    <div id="audio-player-title-area">
+        <div class="click-area">
+            <div id="now-playing"></div>
+        </div>
+    </div>
+  </div>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+  <script src="../js/audioplayer.js"></script>
+
 <!---End Audio Player----->
