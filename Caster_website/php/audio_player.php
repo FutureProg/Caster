4e79fd51a69242b9e48998947185d93b362f563d
Nickolas A. Morrison <nickmorrison09@gmail.com>
2015-12-23 15:40:21 -0500

    Redesigning the audio player

--- Caster_website/php/audio_player.php
+++ Caster_website/php/audio_player.php
@@ -31,5 +31,6 @@
   </div>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
   <script src="../js/audioplayer.js"></script>
+</div>
 
 <!---End Audio Player----->
