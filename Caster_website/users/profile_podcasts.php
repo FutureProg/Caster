efbf7272f357b91bf368bd5d5d326424c614ea29
Nickolas A. Morrison <nickmorrison09@gmail.com>
2015-12-22 20:56:30 -0500

    Working on RSS Feeds

--- Caster_website/users/profile_podcasts.php
+++ Caster_website/users/profile_podcasts.php
@@ -7,7 +7,7 @@ if(filter_input(INPUT_POST,"q") == "PDCST"){
 }
 
 //podcast podcast_id, image, title, description, user_id, username
-function createPodcastBar($podcast,$username){    
+function createPodcastBar($podcast,$username){
     echo "<div class=podcast-bar>";
     echo "<div class='image-container' onclick=playSound($podcast[0],'".str_replace(' ','\%20',addslashes($podcast['title']))."');>";
     echo "<img src='/users/".$podcast[1]."/images/podcast/".$podcast[7]."'/>";
@@ -16,12 +16,12 @@ function createPodcastBar($podcast,$username){
     echo "<div class='description'>";
     echo "<p>$podcast[5]<p><br/>";
     if(filter_input(INPUT_POST,"opt") != null && filter_input(INPUT_POST,"opt") == "MYPROF"){
-        echo "<p>by: <a href='profile.php?user=$podcast[1]'>$username</a><br/> length: ".secondsToTime($podcast)."<br/> uploaded ".dateDifference($podcast)." ago <br/><br/>";    
+        echo "<p>by: <a href='profile.php?user=$podcast[1]'>$username</a><br/> length: ".secondsToTime($podcast)."<br/> uploaded ".dateDifference($podcast)." ago <br/><br/>";
         echo "<a href='/edit.php?id=".$podcast[0]."'>Edit</a>";
         echo "</p>";
     }
     else{
-        echo "<p>by: <a href='profile.php?user=$podcast[1]'>$username</a><br/> length: ".secondsToTime($podcast)."<br/> uploaded ".dateDifference($podcast)." ago</p>";     
+        echo "<p>by: <a href='profile.php?user=$podcast[1]'>$username</a><br/> length: ".secondsToTime($podcast)."<br/> uploaded ".dateDifference($podcast)." ago</p>";
     }
     echo "</div>";
     echo "</div>";
@@ -31,12 +31,12 @@ function loadPodcasts(){
     $userid = filter_input(INPUT_POST,"u");
     $link = mysqli_connect(DB_HOST,DB_USER,DB_PASSWORD,DB_NAME) or die("Unable to connect to the database");
     $query = "SELECT * FROM `".TABLE_PODCASTS."` WHERE `user_id`=".$userid;
-    $result = mysqli_query($link,$query) or die("Error querying the database: ".mysqli_errno($link)."=>".mysqli_error($link));    
+    $result = mysqli_query($link,$query) or die("Error querying the database: ".mysqli_errno($link)."=>".mysqli_error($link));
     mysqli_close($link);
-    $username = username($userid);    
-    if(mysqli_num_rows($result) >= 1){                
+    $username = username($userid);
+    if(mysqli_num_rows($result) >= 1){
         while($row = mysqli_fetch_array($result)){
-            createPodcastBar($row,$username);            
+            createPodcastBar($row,$username);
         }
     }
 }
@@ -73,7 +73,12 @@ function dateDifference($podcast){
         return $delta->m." month".$end;
     }
     else if($delta->d == 0){
-        return "under 24 hours";
+      if($delta->h > 0){
+        $end = "";
+        if($delta->d > 1) $end="s";
+        return $delta->h." hour".$end;
+      }
+      return "under an hour";
     }
     else{
         $end = "";
@@ -82,9 +87,9 @@ function dateDifference($podcast){
     }
 }
 
-function username($userid){    
+function username($userid){
     $link = mysqli_connect(DB_HOST,DB_USER,DB_PASSWORD,DB_NAME) or die("Error connecting to server");
-    $query = "SELECT * FROM `".TABLE_USERS."` WHERE `user_id`=".$userid;    
+    $query = "SELECT * FROM `".TABLE_USERS."` WHERE `user_id`=".$userid;
     $result = mysqli_query($link,$query) or die("Error querying database");
     if(mysqli_num_rows($result) >= 1){
         $row = mysqli_fetch_array($result);
