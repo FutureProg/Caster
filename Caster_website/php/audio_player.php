752a8d822628ac7ec460452d48157f7f4c58c5b2
Nickolas A. Morrison <nickmorrison09@gmail.com>
2015-12-23 16:05:10 -0500

    Redesigning the audio player

--- Caster_website/php/audio_player.php
+++ Caster_website/php/audio_player.php
@@ -14,18 +14,20 @@
     </div>
   <div id="audio-bottom-bar">
     <image id="podcast-art"></image>
-    <div id="audio-player-title-area">
-        <div class="click-area">
-            <div id="now-playing"></div>
-        </div>
-    </div>
-    <button id="play-button"><img src="/images/play_button.png"/></button>
-    <div id="audio-player-scrubber">
-        <div id="audio-player-time-stamp">0:00/0:00</div>
-        <div id="audio-scrub-bar">
-            <div id="audio-scrub-circle">
-            </div>
-        </div>
+    <div>
+      <div id="audio-player-title-area">
+          <div class="click-area">
+              <div id="now-playing"></div>
+          </div>
+      </div>
+      <button id="play-button"><img src="/images/play_button.png"/></button>
+      <div id="audio-player-scrubber">
+          <div id="audio-player-time-stamp">0:00/0:00</div>
+          <div id="audio-scrub-bar">
+              <div id="audio-scrub-circle">
+              </div>
+          </div>
+      </div>      
     </div>
   </div>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
