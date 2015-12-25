6b78ebb89cfb4025e41c738fd3a49bfcd602ba6d
Nickolas A. Morrison <nickmorrison09@gmail.com>
2015-12-24 19:52:00 -0500

    Podcast direct links

--- Caster_website/php/page_content/profile.php
+++ Caster_website/php/page_content/profile.php
@@ -1,23 +1,23 @@
-<?php 
+<?php
 include_once 'phpreq/start_session.php';
 $profile_id = filter_input(INPUT_GET,"user");
 $myprofile =  (isset($_SESSION['user_id']) && $profile_id == $_SESSION['user_id']);
 ?>
 <div id="content-container" style="margin:10px;margin-left:0;top:90px;height:80%;">
-    <div id="profile-page">        
-        <div id="profile-left-bar">             
+    <div id="profile-page">
+        <div id="profile-left-bar">
             <label id="profile-image-container">
                 <img id="profile-image">
-                <img id="profile-image-camera" src="images/edit_camera.png"/>
+                <img id="profile-image-camera" src="/images/edit_camera.png"/>
                 <input id="my-profile-image-uploader" name="new_profile_image" accept="image/png, image/jpg, image/jpeg" type="file" style="display:none;"/>
-                </img>  
+                </img>
             </label>
         <div id="profile-user-name" class="circle-header">
             <h1></h1>
-        </div>           
+        </div>
         <div id="profile-subscribe-button">
-            <img class="profile-subscribe-button-image" src="images/subscribe_button.png"/>
-        </div>                  
+            <img class="profile-subscribe-button-image" src="/images/subscribe_button.png"/>
+        </div>
         <p id="profile-description">
             <!-- Description: Lorem ipsum dolor sit amet,
 duis et eu vitae, diam optio donec at dolor, eleifend eu volutpat                                   suspendisse, <a href="">nulla magna</a> elit. -->
@@ -25,19 +25,19 @@ duis et eu vitae, diam optio donec at dolor, eleifend eu volutpat               
         <br/>
         <a href="settings.php" id="settings-link">settings</a>
     </div>
-    <div id="profile-content">                                        
+    <div id="profile-content">
         <!--<nav id="profile-nav-bar">
 <a href="" id="profile-activity-item" style="border-left: 1px solid;">Activity</a>
 <a href="" id="profile-podcasts-item">Podcasts</a>
 <a href="" id="profile-playlists-item">Playlists</a>
-</nav>-->                 
-        <div id="profile-activity-area">                                                                
+</nav>-->
+        <div id="profile-activity-area">
         </div>
     </div>
 </div>
-<?php include '../../phpreq/footer.php'; ?>          
+<?php include '../../phpreq/footer.php'; ?>
 <script type='text/javascript'>
     document.title = "Caster - Profile";
 </script>
-<script src="js/profile.js"></script> 
+<script src="/js/profile.js"></script> 
 </div>
--- Caster_website/profile.php
+++ Caster_website/profile.php
@@ -20,50 +20,50 @@ and open the template in the editor.
         <title>Caster-Profile</title>
     </head>
     <body>
-        <?php include 'php/audio_player.php'; ?>
+        <?php include '/php/audio_player.php'; ?>
         <div id="body-container">
-            <?php include 'phpreq/topbar.php'?>
+            <?php include '/phpreq/topbar.php'?>
             <div id="main-content">
                 <div id="content-container" style="margin:10px;margin-left:0;top:90px;height:80%;">
-    <div id="profile-page">        
-            <div id="profile-left-bar">             
+    <div id="profile-page">
+            <div id="profile-left-bar">
                 <label id="profile-image-container">
                     <img id="profile-image">
-                        <img id="profile-image-camera" src="images/edit_camera.png"/>
+                        <img id="profile-image-camera" src="/images/edit_camera.png"/>
                         <input id="my-profile-image-uploader" name="new_profile_image" accept="image/png, image/jpg, image/jpeg" type="file" style="display:none;"/>
-                    </img>  
+                    </img>
                 </label>
                 <div id="profile-user-name" class="circle-header">
                     <h1></h1>
-                </div>                                
+                </div>
             <div id="profile-subscribe-button">
-                <img class="profile-subscribe-button-image" src="images/subscribe_button.png"/>
-            </div>                  
+                <img class="profile-subscribe-button-image" src="/images/subscribe_button.png"/>
+            </div>
                 <p id="profile-description">
                    <!-- Description: Lorem ipsum dolor sit amet,
                     duis et eu vitae, diam optio donec at dolor, eleifend eu volutpat                                   suspendisse, <a href="">nulla magna</a> elit. -->
                 </p><br/>
                 <a href="settings.php" id="settings-link">settings</a>
             </div>
-            <div id="profile-content">                                        
+            <div id="profile-content">
                 <!--<nav id="profile-nav-bar">
                     <a href="" id="profile-activity-item" style="border-left: 1px solid;">Activity</a>
                     <a href="" id="profile-podcasts-item">Podcasts</a>
                     <a href="" id="profile-playlists-item">Playlists</a>
-                </nav>-->                 
-                <div id="profile-activity-area">                                                                
+                </nav>-->
+                <div id="profile-activity-area">
                 </div>
             </div>
         </div>
-        <?php include 'phpreq/footer.php'; ?>          
+        <?php include '/phpreq/footer.php'; ?>
         <script type='text/javascript'>
             document.title = "Caster - Profile";
         </script>
-    <script src="js/profile.js"></script> 
+    <script src="/js/profile.js"></script>
     </div>
 </div>
-            </div>            
-        </div>    
-        <script src="js/main.js"></script>        
+            </div>
+        </div>
+        <script src="/js/main.js"></script>
     </body>
 </html>
