d8bce31b885c0affe12205674ea2087444b80f7c
Nickolas A. Morrison <nickmorrison09@gmail.com>
2016-06-27 21:30:54 -0400

    Updated the ignore

--- Caster_website/upload.php
+++ Caster_website/upload.php
@@ -12,7 +12,26 @@ include 'phpreq/start_session.php';
     </head>
     <body>
         <div id="body-container">
-            <?php include 'phpreq/topbar.php';?>
+            <div id="top-bar">
+                <a href='index.php' class='inner-link'><img src="../images/title.png"/></a>
+                <div id="search-form">
+                    <label>
+                        <input id="search-box" class="tb" size="50" type="text"/>
+                    </label>
+                    <button id="search-button" type="submit">Search</button>
+                </div>
+                <div id="button-set" style="margin-right: 10px;">
+                    <button class="top-bar-buttons" onclick="window.location.href='opinion.php';" style="margin-left:50px">!?   </button>
+                    <?php if(!isset($_SESSION['user_id'])){ ?>
+                            <button class="top-bar-buttons" onclick="window.location.href='login.php';" style="margin-left:50px;">Log in</button>
+                            <button class="top-bar-buttons" onclick="window.location.href='signup.php'" style="margin-left:50px;">Sign up</button>
+                    <?php }else{?>
+                            <button class="top-bar-buttons" onclick="/<?php echo $_SESSION['username'];?>'" ><?php echo $_SESSION['username']; ?></button>
+                            <button class="top-bar-buttons" onclick="window.location.href='/upload.php'">Upload</button>
+                            <button class="top-bar-buttons" onclick="window.location.href='../php/signout.php'">Sign Out</button>
+                    <?php } ?>
+                </div>
+            </div>
             <div id="main-content">
                 <div id="content-container" style="margin:10px;margin-left:0;top:90px;height:80%;">
                     <div id="upload-container">
